1b9e13f41bbe9ab073802c377706bc30c89a36bf
Annisa Puspitasari <nisa.codelifings@gmail.com>
2020-11-27 01:30:13 -0800

    ok

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Jasa Deploy Website Statis</h1>
-      <p>Source Code : Reactjs, dan html.</p>
-      <h5 className="motto">-Anda bayar saat website telah terdeploy-</h5>
-     <Content />
+      <div className="header">
+      <img className="logo" src="https://raw.githubusercontent.com/nisacodelifings/Web-Olshop-Zasiera/e3c15b9843cc851e2b37cfac7e17ba7853a41b3b/images/lotus.png" />
+      <h1>Zasiera</h1>
+      <h4>-Mukena Sahabat Muslimah-</h4>
+      <h5 className="motto">by Susylawati Pandansari</h5>
+  </div>
+      <Content />
     </div>
   );
 }
--- src/Content.js
+++ src/Content.js
@@ -3,22 +3,56 @@ import React from "react";
 export default function Content() {
     return (
         <main>
-        <p>Deployment adalah Proses pemasangan sebuah website ke Internet yang nantinya dapat diakses oleh siapa saja.</p>
-			<h3>Tidak usah bingung lagi, Pasang hosting website anda Sekarang Disini!!</h3> 
-			<h2>Anda akan mendapatkan domain gratis!</h2>
-			<p>Di dunia IT, Jasa ini biasa disebut sebagai DevOps.
-			Anda hanya harus cari developer pembuat website anda yang nantinya memberikan anda sebuah source code baik itu 
-			adalah reactjs atau html, dan kami siap melakukan operator website anda menjadi online di internet. </p>
-			<h2>Domain Gratis?</h2>
-			<h4>Ya, Gratis Forever!! dan akan Terpantau langsung di Google!!</h4>
-			<h3>Yakin, gamau?</h3>
-      		<h2>Masalah Harga?</h2>
-      		<h4>Ga perlu khawatir, Harga Operasional kami standar kok! Tanya2 dulu saja kepada operator whatsapp kami dulu yaaa..</h4>
-      		<h2>Keunggulan Kami?</h2>
-      		<p>Anda hanya bayar 1x saja seumur hidup, Dapatkan Hosting tanpa harus langganan tiap Tahun-nya, Dapatkan Domain Gratis terdeteksi Google, Harga standar dan terpercaya!!!</p>
-      		<h1>Tunggu apa lagi?</h1>
-      		<h2>Miliki Hosting anda Sekarang!!!</h2>
-      		<a href="https://wa.me/6289508320163"><button>Order</button></a>
+			<img className="img" src="https://res.cloudinary.com/codelifings/image/upload/v1590977850/Tak_berjudul79-1_dzjcsg.jpg" />
+<p>Harga Normal Rp. <b class="conteng">950k</b> diskon jadi Rp. <b>875k</b> dan sekarang, Ada penawaran Spesial untuk anda menjadi</p>
+                                <h2><span class="harga">Rp. 775.000</span></h2>
+								<h2>Beli banyak lebih HEMAT!</h2>
+<h4>2Pcs Mukena Hanya Rp. <b class="conteng">155k</b> 1450k</h4>
+<h4>Lebih hemat Rp. 100k</h4>
+
+<h1>Mengenai Produk</h1>
+<p class="produk">1. Berat 1 pcs mukena : 800 gram. <br />
+2. Cutting Mukena : Mukena setelan atas bawah, Renda strecht di sekeliling muka sampai sampai dada. <br />
+3. Ukuran Atasan Mukena : Panjang Depan : 118 cm,
+Panjang Belakang : 130 cm <br />
+4. Ukuran Bawahan Mukena : Panjang 120 cm, Lingkar Rok: 71 cm<br /> 
+</p>
+<h2>
+	Tersedia 5 Macam warna:
+	</h2>
+
+	<img src="https://res.cloudinary.com/codelifings/image/upload/v1590983365/2020-06-01_09.04.37_1_gp23jd.jpg" alt="" />
+	<h4>Putih 01</h4>
+	<img src="https://res.cloudinary.com/codelifings/image/upload/v1590983365/2020-06-01_09.03.56_1_ryuy7k.jpg" alt="" />
+	<h4>Mocca 44</h4>
+
+	<img src="https://res.cloudinary.com/codelifings/image/upload/v1590983364/2020-06-01_09.04.07_1_pkby1x.jpg" alt="" />
+	<h4>Hitam 35</h4>
+	<img src="https://res.cloudinary.com/codelifings/image/upload/v1590983364/2020-06-01_09.04.16_1_itznbw.jpg" alt="" />
+	<h4>Hijau muda 37</h4>
+	<img src="https://res.cloudinary.com/codelifings/image/upload/v1590983364/2020-06-01_09.04.27_1_uso7jv.jpg" alt="" />
+	<h4>Dusty 41</h4>
+
+<h3>Mau warna lain? BISA!!!</h3>
+<h2>PRE-Order 1 Minggu</h2>
+<img className="img" src="https://raw.githubusercontent.com/nisacodelifings/Web-Olshop-Zasiera/e3c15b9843cc851e2b37cfac7e17ba7853a41b3b/images/color.jpeg" />
+
+<br />
+<br />
+<q>Mungkin ada perbedaan warna antara Foto dan Asli sekitar 10%, Karena tangkapan kamera dan pencahayaan di studio foto</q>
+
+<h2>Kenapa harus pesan di Toko Kami?</h2>
+<p>dibuat dari bahan yg TERBAIK, Jahitan dan Finishing kualitas Butik, (Rapi, Elegant, Awet) serta Quality Control</p>        
+      		<h4>Kami pastikan paket yang anda terima sudah dalam pengecekan terlebih dahulu</h4>
+			  <h2>100% Amanah & Terpercaya</h2>
+			  <p>karena sudah ribuan yang Order di Toko online kami <br /> Jika produk diterima dalam keadaan Cacat maka, kami ganti yang baru.</p>
+			  <h3>Pembayaran COD dan Transfer atau Pilih yg Memudahkan Bunda saja.</h3>
+			  <p>Semua Rekening bank a/n satu orang, Kecil kemungkinan penipuan. Pemesanan hanya melalui online, Saat ini kami tidak  membuka toko offline di manapun.</p>
+			  
+			  <h2>YuK, Buruan Serbu Sekarang!!</h2>
+			  <p>KAMI Khawatir Ukhty TIDAK KEBAGIAN <br/> Jika tidak PESAN SEKARANG</p>
+
+      		<a href="https://wa.me/6281225420505"><button>Order</button></a>
 
       		<br />
       		<br />
