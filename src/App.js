5a0707ec8f8bcfafab846e5781352fbb3ec0da44
Annisa Puspitasari <nisa.codelifings@gmail.com>
2021-01-09 05:45:58 -0800

    alhamdulillah

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ export default function App() {
     <div className="App">
       <div className="header">
       <img className="logo" src="https://raw.githubusercontent.com/nisacodelifings/Web-Olshop-Zasiera/e3c15b9843cc851e2b37cfac7e17ba7853a41b3b/images/lotus.png" />
-      <h1>Zasiera</h1>
+      <h1 class="harga">Zasiera</h1>
       <h4>-Mukena Sahabat Muslimah-</h4>
       <h5 className="motto">by Susylawati Pandansari</h5>
+<a style={{color:"white", textDecoration:"none"}} class="harga" href="https://www.instagram.com/mukena.zasiera/">Follow instagram kami</a>
   </div>
       <Content />
     </div>
--- src/Content.js
+++ src/Content.js
@@ -6,17 +6,20 @@ export default function Content() {
 			<img className="img" src="https://res.cloudinary.com/codelifings/image/upload/v1590977850/Tak_berjudul79-1_dzjcsg.jpg" />
 <p>Harga Normal Rp. <b class="conteng">950k</b> diskon jadi Rp. <b>875k</b> dan sekarang, Ada penawaran Spesial untuk anda menjadi</p>
                                 <h2><span class="harga">Rp. 775.000</span></h2>
-								<h2>Beli banyak lebih HEMAT!</h2>
-<h4>2Pcs Mukena Hanya Rp. <b class="conteng">155k</b> 1450k</h4>
-<h4>Lebih hemat Rp. 100k</h4>
-
-<h1>Mengenai Produk</h1>
-<p class="produk">1. Berat 1 pcs mukena : 800 gram. <br />
-2. Cutting Mukena : Mukena setelan atas bawah, Renda strecht di sekeliling muka sampai sampai dada. <br />
-3. Ukuran Atasan Mukena : Panjang Depan : 118 cm,
-Panjang Belakang : 130 cm <br />
-4. Ukuran Bawahan Mukena : Panjang 120 cm, Lingkar Rok: 71 cm<br /> 
-</p>
+								<h2 class="harga">Beli banyak lebih HEMAT!</h2>
+<div class="harga">
+<h2>2Pcs Mukena Hanya Rp. <b class="conteng">1550k</b> 1450k</h2>
+<h2>Lebih hemat Rp. 100k, bukan?</h2>
+</div>
+<br />
+<h1>Mengenai Produk : </h1>
+<ul class="produk harga"><br />
+	<li class="harga">Berat 1 pcs mukena : 800 gram. </li> <br />
+	<li class="harga">Mukena setelan atas bawah, Renda strecht di sekeliling muka sampai sampai dada. </li> <br />
+	<li class="harga">Ukuran Atasan Mukena : Panjang Depan : 118 cm, 
+Panjang Belakang : 130 cm </li> <br />
+	<li class="harga">Ukuran Bawahan Mukena : Panjang 120 cm, Lingkar Rok: 71 cm </li><br /> 
+</ul>
 <h2>
 	Tersedia 5 Macam warna:
 	</h2>
@@ -33,22 +36,34 @@ Panjang Belakang : 130 cm <br />
 	<img src="https://res.cloudinary.com/codelifings/image/upload/v1590983364/2020-06-01_09.04.27_1_uso7jv.jpg" alt="" />
 	<h4>Dusty 41</h4>
 
-<h3>Mau warna lain? BISA!!!</h3>
-<h2>PRE-Order 1 Minggu</h2>
+	<br />
+<div class="harga">
+<h1>Mau warna lain?</h1>
+<h2 class="harga">PRE-Order 1 Minggu!!</h2>
+</div>
 <img className="img" src="https://raw.githubusercontent.com/nisacodelifings/Web-Olshop-Zasiera/e3c15b9843cc851e2b37cfac7e17ba7853a41b3b/images/color.jpeg" />
 
 <br />
-<br />
-<q>Mungkin ada perbedaan warna antara Foto dan Asli sekitar 10%, Karena tangkapan kamera dan pencahayaan di studio foto</q>
 
-<h2>Kenapa harus pesan di Toko Kami?</h2>
-<p>dibuat dari bahan yg TERBAIK, Jahitan dan Finishing kualitas Butik, (Rapi, Elegant, Awet) serta Quality Control</p>        
+<br />
+<br />
+<h1>Sebelum itu.........</h1>
+<h3>Kita mengetahui bahwa sekarang memang banyak yg menjual Mukena dimana-mana, Namun...</h3>
+<h3 class="harga">Apa yg membedakan kami dengan yg lain?</h3>
+<p class="harga">Pertama, Bahan  dibuat dari yg TERBAIK, Jahitan dan Finishing kualitas Butik, (Rapi, Elegant, Awet) serta Quality Control</p>        
       		<h4>Kami pastikan paket yang anda terima sudah dalam pengecekan terlebih dahulu</h4>
-			  <h2>100% Amanah & Terpercaya</h2>
-			  <p>karena sudah ribuan yang Order di Toko online kami <br /> Jika produk diterima dalam keadaan Cacat maka, kami ganti yang baru.</p>
+			<img className="img" src="https://res.cloudinary.com/codelifings/image/upload/v1590977850/Tak_berjudul79-1_dzjcsg.jpg" />
+			 
+			  <h2 class="harga">100% Amanah & Terpercaya</h2>
+			  <p class="harga">karena sudah ribuan yang Order di Toko online kami <br /> Jika produk diterima dalam keadaan Cacat maka, kami ganti yang baru.</p>
 			  <h3>Pembayaran COD dan Transfer atau Pilih yg Memudahkan Bunda saja.</h3>
 			  <p>Semua Rekening bank a/n satu orang, Kecil kemungkinan penipuan. Pemesanan hanya melalui online, Saat ini kami tidak  membuka toko offline di manapun.</p>
-			  
+				<div className="header">
+      <img className="logo" src="https://raw.githubusercontent.com/nisacodelifings/Web-Olshop-Zasiera/e3c15b9843cc851e2b37cfac7e17ba7853a41b3b/images/lotus.png" />
+      <h1 class="harga">Zasiera</h1>
+      <h4>-Mukena Sahabat Muslimah-</h4>
+      <h5 className="motto">by Susylawati Pandansari</h5>
+  </div>
 			  <h2>YuK, Buruan Serbu Sekarang!!</h2>
 			  <p>KAMI Khawatir Ukhty TIDAK KEBAGIAN <br/> Jika tidak PESAN SEKARANG</p>
 
