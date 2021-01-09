de5e7d71d9d6bb68edc004f22a388ec766e10e46
Annisa Puspitasari <nisa.codelifings@gmail.com>
2021-01-09 06:25:56 -0800

    ok

--- src/Content.js
+++ src/Content.js
@@ -4,26 +4,29 @@ export default function Content() {
     return (
         <main>
 			<img className="img" src="https://res.cloudinary.com/codelifings/image/upload/v1590977850/Tak_berjudul79-1_dzjcsg.jpg" />
-<p>Harga Normal Rp. <b class="conteng">950k</b> diskon jadi Rp. <b>875k</b> dan sekarang, Ada penawaran Spesial untuk anda menjadi</p>
-                                <h2><span class="harga">Rp. 775.000</span></h2>
+<h2>Harga Normal Rp. <b class="conteng">950k</b> <br />Ada penawaran Spesial untuk anda</h2>
+                                <h2>menjadi <span class="harga">Rp. 775.000</span></h2>
 								<h2 class="harga">Beli banyak lebih HEMAT!</h2>
 <div class="harga">
 <h2>2Pcs Mukena Hanya Rp. <b class="conteng">1550k</b> 1450k</h2>
 <h2>Lebih hemat Rp. 100k, bukan?</h2>
 </div>
 <br />
+<a href="https://wa.me/6281225420505"><button>Order</button></a>
+
+<br />
 <h1>Mengenai Produk : </h1>
-<ul class="produk harga"><br />
+<div class="produk harga"><br />
 	<li class="harga">Berat 1 pcs mukena : 800 gram. </li> <br />
 	<li class="harga">Mukena setelan atas bawah, Renda strecht di sekeliling muka sampai sampai dada. </li> <br />
 	<li class="harga">Ukuran Atasan Mukena : Panjang Depan : 118 cm, 
 Panjang Belakang : 130 cm </li> <br />
 	<li class="harga">Ukuran Bawahan Mukena : Panjang 120 cm, Lingkar Rok: 71 cm </li><br /> 
-</ul>
+</div>
 <h2>
 	Tersedia 5 Macam warna:
 	</h2>
-
+<div class="harga">
 	<img src="https://res.cloudinary.com/codelifings/image/upload/v1590983365/2020-06-01_09.04.37_1_gp23jd.jpg" alt="" />
 	<h4>Putih 01</h4>
 	<img src="https://res.cloudinary.com/codelifings/image/upload/v1590983365/2020-06-01_09.03.56_1_ryuy7k.jpg" alt="" />
@@ -35,38 +38,47 @@ Panjang Belakang : 130 cm </li> <br />
 	<h4>Hijau muda 37</h4>
 	<img src="https://res.cloudinary.com/codelifings/image/upload/v1590983364/2020-06-01_09.04.27_1_uso7jv.jpg" alt="" />
 	<h4>Dusty 41</h4>
-
+</div>
 	<br />
 <div class="harga">
 <h1>Mau warna lain?</h1>
 <h2 class="harga">PRE-Order 1 Minggu!!</h2>
 </div>
-<img className="img" src="https://raw.githubusercontent.com/nisacodelifings/Web-Olshop-Zasiera/e3c15b9843cc851e2b37cfac7e17ba7853a41b3b/images/color.jpeg" />
+<img className="img harga" src="https://raw.githubusercontent.com/nisacodelifings/Web-Olshop-Zasiera/e3c15b9843cc851e2b37cfac7e17ba7853a41b3b/images/color.jpeg" />
 
 <br />
+<br />
+<a href="https://wa.me/6281225420505"><button>Order</button></a>
 
 <br />
 <br />
 <h1>Sebelum itu.........</h1>
 <h3>Kita mengetahui bahwa sekarang memang banyak yg menjual Mukena dimana-mana, Namun...</h3>
-<h3 class="harga">Apa yg membedakan kami dengan yg lain?</h3>
-<p class="harga">Pertama, Bahan  dibuat dari yg TERBAIK, Jahitan dan Finishing kualitas Butik, (Rapi, Elegant, Awet) serta Quality Control</p>        
-      		<h4>Kami pastikan paket yang anda terima sudah dalam pengecekan terlebih dahulu</h4>
+<h2 class="harga">Apa yang membedakan kami dengan yang lain?</h2>
+<h2 class="harga">Pertama, Bahan  dibuat dari yg TERBAIK, Jahitan dan Finishing kualitas Butik, (Rapi, Elegant, Awet) serta Quality Control</h2>        
+<br />
+					<h2>Kami pastikan paket yang anda terima sudah dalam pengecekan terlebih dahulu!!!</h2>
+					<br />
+		
 			<img className="img" src="https://res.cloudinary.com/codelifings/image/upload/v1590977850/Tak_berjudul79-1_dzjcsg.jpg" />
 			 
 			  <h2 class="harga">100% Amanah & Terpercaya</h2>
-			  <p class="harga">karena sudah ribuan yang Order di Toko online kami <br /> Jika produk diterima dalam keadaan Cacat maka, kami ganti yang baru.</p>
-			  <h3>Pembayaran COD dan Transfer atau Pilih yg Memudahkan Bunda saja.</h3>
-			  <p>Semua Rekening bank a/n satu orang, Kecil kemungkinan penipuan. Pemesanan hanya melalui online, Saat ini kami tidak  membuka toko offline di manapun.</p>
+			 <div class="harga">
+			 <h2>Pembayaran COD, Transfer atau Pilih yang Memudahkan Bunda saja.</h2>
+			  <h3>Semua Rekening bank a/n satu orang, Kecil kemungkinan penipuan. Pemesanan hanya melalui online, Saat ini kami tidak  membuka toko offline di manapun.</h3>
+				</div>
+				<div class="harga">
+			  <h2>YuK, Buruan Serbu Sekarang!!</h2>
+			  <p>KAMI Khawatir Ukhty TIDAK KEBAGIAN <br/> Jika tidak PESAN SEKARANG</p>
+</div>
+<br/>
 				<div className="header">
       <img className="logo" src="https://raw.githubusercontent.com/nisacodelifings/Web-Olshop-Zasiera/e3c15b9843cc851e2b37cfac7e17ba7853a41b3b/images/lotus.png" />
       <h1 class="harga">Zasiera</h1>
       <h4>-Mukena Sahabat Muslimah-</h4>
       <h5 className="motto">by Susylawati Pandansari</h5>
   </div>
-			  <h2>YuK, Buruan Serbu Sekarang!!</h2>
-			  <p>KAMI Khawatir Ukhty TIDAK KEBAGIAN <br/> Jika tidak PESAN SEKARANG</p>
-
+	
       		<a href="https://wa.me/6281225420505"><button>Order</button></a>
 
       		<br />
